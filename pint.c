b2696fe20a2e919b76eafa780a2e7d3a295bcfd7
Sibongile <sibongile.nhlema@gmail.com>
2023-11-14 08:41:54 +0000

    created pint function for task 1

---
+++ pint.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pint - prints the value at the top of the stack, followed by a \n
+ * @stack: double pointer to the stack
+ * @unsigned int line_number: number on which the line is being read
+ */
+
+void _pint_func(stack_t **stack, unsigned int line_number)
+{
+	/*check if stack if empty */
+	if ((*stack) == NULL)
+	{
+		fprintf(stderr, "%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);	
+	/*find head of the linked list*/
+	/*use stack data structure*/
+	/*does not add or remove elements*/
+	/*so no need to change prev or next values*/
+	/*LIFO*/
+}
