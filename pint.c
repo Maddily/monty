92631e852088d06183d7cedcec1b6450e3ffcaed
Sibongile <sibongile.nhlema@gmail.com>
2023-11-14 08:48:09 +0000

    fixed betty errors

--- pint.c
+++ pint.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * pint - prints the value at the top of the stack, followed by a \n
+ * _pint_func - prints the value at the top of the stack, followed by a \n
  * @stack: double pointer to the stack
- * @unsigned int line_number: number on which the line is being read
+ * @line_number: number on which the line is being read
  */
 
 void _pint_func(stack_t **stack, unsigned int line_number)
@@ -14,7 +14,7 @@ void _pint_func(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "%d: can't pint, stack empty", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);	
+	printf("%d\n", (*stack)->n);
 	/*use stack data structure*/
 	/*does not add or remove elements*/
 	/*so no need to change prev or next values*/
---
+++ pop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * _pop_func - removes the top element of the stack
+ * @stack: double pointer to the stack
+ * @line_number: line being read
+ */
+
+void _pop_func(stack_t **stack, unsigned int line_number)
+{
+}
