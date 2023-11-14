5cc5c4f5f05ce1835ee52bfe25a3dea83ea726ef
Sibongile <sibongile.nhlema@gmail.com>
2023-11-14 09:10:12 +0000

    created top function for task 2.

--- pop.c
+++ pop.c
@@ -8,4 +8,31 @@
 
 void _pop_func(stack_t **stack, unsigned int line_number)
 {
+	stack_t *element;
+
+	if ((*stack) == NULL)
+	{
+		fprintf(stderr, "%d: can't pop an empty stack", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	element = (*stack);
+	/*right now top:*/
+	/*top->next = NULL*/
+	/*top->prev = element->prev*/
+	/*top->n = element->n*/
+
+	/*element before top*/
+	/*top->prev->next = NULL*/
+	/*top->prev->prev = top->prev->prev->prev*/
+
+	/*no need to update prev value of element before top*/
+	(*stack) = element->prev;
+	if ((*stack) != NULL)
+		element->prev->next = NULL;
+	free(element);
+	/*find element on the top of the list*/
+	/*change prev and next*/
+	/*remove top element*/
+	/*LIFO*/
 }
